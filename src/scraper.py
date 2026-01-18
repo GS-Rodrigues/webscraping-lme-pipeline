58165904e61611497e6d52a4ab7487040b52a8d0
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2026-01-18 15:19:37 -0300

    Update(sctaper.py)

--- src/scraper.py
+++ src/scraper.py
@@ -4,30 +4,51 @@ from datetime import date
 import date_treatment
 from bs4 import BeautifulSoup
 
-url = f"https://shockmetais.com.br/lme/{date.today().month}-{date.today().year}";
+j = 2008
 
-response = requests.get(url);
+month_to_year = {
+    "jan": 1,
+    "fev": 2,
+    "mar": 3,
+    "abr": 4,
+    "mai": 5,
+    "jun": 6,
+    "jul": 7,
+    "ago": 8,
+    "set": 9,
+    "out": 10,
+    "nov": 11,
+    "dez": 12
+}
 
-html_content = response.content;
-
-
-soup = BeautifulSoup(html_content, 'html.parser');
-
-table = soup.find_all(
-    name='table',
-    attrs= {'class' : 'table table-hover table-sm table-striped shadow'}
-)[0];
-
-
-for row in table.find_all('tr')[1:-1]:
-    colms = row.find_all('td');
-    if colms:
-        month_year = colms[0].text.strip();
-        raw = colms[3].text.strip()
-        clean = raw.replace(" ", "").replace("\n", "").replace(",", "").replace(",", ".")
-        aluminium = float(clean)
-        
-        if "Média" in month_year:
-            continue;
-        else:
-            Insert.insert_row(date_treatment.parse_data_br(month_year, date.today().year), aluminium);
+while j <= date.today().year:
+    i = 1;
+    while i <= 12:
+        if not (j == date.today().year and i > date.today().month):
+            url = f"https://shockmetais.com.br/lme/{i}-{j}";
+            
+            response = requests.get(url);
+            
+            html_content = response.content;
+            
+            soup = BeautifulSoup(html_content, 'html.parser');
+            
+            table = soup.find_all(
+                name='table',
+                attrs= {'class' : 'table table-hover table-sm table-striped shadow'}
+            )[0];
+            
+            for row in table.find_all('tr')[1:-1]:
+                colms = row.find_all('td');
+                if colms:
+                    month_year = colms[0].text.strip();
+                    raw_number = colms[3].text.strip();
+                    clean_number = raw_number.replace(" ", "").replace("\n", "").replace(",", "").replace(",", ".");
+                    aluminium = clean_number;
+                    if ((not ("feriado" in aluminium)) and (not ("Média" in month_year))):
+                        aluminium = float(aluminium);
+                        Insert.insert_row(date_treatment.parse_data_br(month_year, j), aluminium);
+                    else:
+                        continue;
+            i = (i + 1);
+    j = (j + 1);
