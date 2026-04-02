4f97462097d18654129a4d216cc678c6b7213f4c
Guilherme da Silva Rodrigues <168394483+GS-Rodrigues@users.noreply.github.com>
2026-04-02 08:17:12 -0300

    Update scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -9,51 +9,50 @@ conn = get_connection();
 cursor = conn.cursor();
 
 try:
-    for i in {1,2,3,4}:
-        # yesterday = (datetime.now() - timedelta(days=1)).date()
-        # data_str = yesterday.strftime("%m-%Y")
-        data_str = f'{i}-2026'
+    yesterday = (datetime.now() - timedelta(days=1)).date()
+    #data_str = yesterday.strftime("%m-%Y")
+    data_str = f'3-2026'
+
+    url = f"https://shockmetais.com.br/lme/{data_str}";
+    response = requests.get(url, timeout=10);
+    response.raise_for_status()
     
-        url = f"https://shockmetais.com.br/lme/{data_str}";
-        response = requests.get(url, timeout=10);
-        response.raise_for_status()
-        
-        html_content = response.content;
-        
-        soup = BeautifulSoup(html_content, 'html.parser');
-        
-        tables = soup.find_all(
-            name='table',
-            attrs= {'class' : 'table table-hover table-sm table-striped shadow'}
-        );
+    html_content = response.content;
     
-        if not tables:
-            raise RuntimeError("Tabela LME não encontrada na página")
+    soup = BeautifulSoup(html_content, 'html.parser');
+    
+    tables = soup.find_all(
+        name='table',
+        attrs= {'class' : 'table table-hover table-sm table-striped shadow'}
+    );
+
+    if not tables:
+        raise RuntimeError("Tabela LME não encontrada na página")
+    else:
+        table = tables[0]
+
+    
+    for row in table.find_all('tr')[1:-1]:
+        colms = row.find_all('td');
+
+        if not colms:
+            continue;
         else:
-            table = tables[0]
-    
-        
-        for row in table.find_all('tr')[1:-1]:
-            colms = row.find_all('td');
-    
-            if not colms:
+            month_year = colms[0].text.strip();
+            if "feriado" in month_year.lower() or "média" in month_year.lower():
                 continue;
-            else:
-                month_year = colms[0].text.strip();
-                if "feriado" in month_year.lower() or "média" in month_year.lower():
-                    continue;
-                
-                raw_value = colms[3].get_text(strip=True)
-    
-                try:
-                    aluminium = float(raw_value.replace(" ", "").replace("\n", "").replace(",", ""));
-                    date_clean = date_treatment.parse_data_br(month_year, yesterday.year);
-                    if date_clean.month == yesterday.month:
-                        aluminium = float(aluminium);
-                        insert_row(cursor, date_clean, aluminium);
-                except ValueError:
-                    continue
-        conn.commit();
+            
+            raw_value = colms[3].get_text(strip=True)
+
+            try:
+                aluminium = float(raw_value.replace(" ", "").replace("\n", "").replace(",", ""));
+                date_clean = date_treatment.parse_data_br(month_year, yesterday.year);
+                if date_clean.month == yesterday.month:
+                    aluminium = float(aluminium);
+                    insert_row(cursor, date_clean, aluminium);
+            except ValueError:
+                continue
+    conn.commit();
 
 except Exception as e:
     conn.rollback()
